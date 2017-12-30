f557b0a650ff424b886e67708c2ca6f9753ed61a
ezio1051404 <s1051404@gm.cyut.edu.tw>
2017-12-30 22:24:18 +0800

    12/30

--- Web/main/views.py
+++ Web/main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-
+from product.models import product
 
 
 def main(request):
     '''
     Show 'Hello world!' in the main page
     '''
-    return render(request, 'main/main.html')
+    products = product.objects.all()
+    context = {'products':products}
+    return render(request, 'main/main.html',context)
